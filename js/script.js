bb804d2fb8bf058f8042f23439909365ccab069e
gioshaverdovi <giorgi.shaverdovi11b@geolab.edu.ge>
2019-01-23 16:16:16 +0400

    js

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$("#ourteamLink").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#ourteam").offset().top
+    }, 500);
+});
+// $("#bandLink").click(function() {
+//     $('html, body').animate({
+//         scrollTop: $("#band").offset().top
+//     }, 500);
+// });
+// $("#photosLink").click(function() {
+//     $('html, body').animate({
+//         scrollTop: $("#potos").offset().top
+//     }, 500);
+// });
+// $("#songsLink").click(function() {
+//     $('html, body').animate({
+//         scrollTop: $("#songs").offset().top
+//     }, 500);
+// });
+// $("#tourLink").click(function() {
+//     $('html, body').animate({
+//         scrollTop: $("#tour").offset().top
+//     }, 500);
+// });
+// $("#contactLink").click(function() {
+//     $('html, body').animate({
+//         scrollTop: $("#contact").offset().top
+//     }, 500);
+// });
